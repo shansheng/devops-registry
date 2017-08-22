babd3b8ec81e0840c7e39b7068902e9ad0eeca87
ZhongWen Li <lao-tse@qq.com>
2017-08-22 14:58:14 +0800

    Add: tomcat and devops dockerfile

---
+++ docker/devops/resources/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2001-2018 Primeton Technologies, Ltd.
+# All rights reserved.
+#
+# author: ZhongWen Li (mailto:lizw@primeton.com)
+#
+
+CONFIG_PATH=${TOMCAT_HOME}/webapps/ROOT/WEB-INF/_srv/config
+
+if [ -z "${MYSQL_DATABASE}" ]; then
+  echo "[`date`] [WARN ] Missing enviroment variable MYSQL_DATABASE, use default value: devops"
+fi
+if [ -z "${MYSQL_USER}" ]; then
+  echo "[`date`] [WARN ] Missing enviroment variable MYSQL_USER, use default value: devops"
+fi
+if [ -z "${MYSQL_PASSWORD}" ]; then
+  echo "[`date`] [WARN ] Missing enviroment variable MYSQL_PASSWORD, use default value: devops"
+fi
+
+if [ -f ${CONFIG_PATH}/user-config-template.xml ]; then
+  \cp -f ${CONFIG_PATH}/user-config-template.xml ${CONFIG_PATH}/user-config.xml
+  sed -i -e "s/MYSQL_DATABASE/${MYSQL_DATABASE}/g" ${CONFIG_PATH}/user-config.xml
+  sed -i -e "s/MYSQL_USER/${MYSQL_USER}/g" ${CONFIG_PATH}/user-config.xml
+  sed -i -e "s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g" ${CONFIG_PATH}/user-config.xml
+fi
+
+${TOMCAT_HOME}/bin/entrypoint.sh
---
+++ docker/tomcat/resources/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2001-2018 Primeton Technologies, Ltd.
+# All rights reserved.
+#
+# author: ZhongWen Li (mailto:lizw@primeton.com)
+#
+
+if [ -z ${TOMCAT_HOME} ]; then
+  echo "[`date`] [ERROR] \${TOMCAT_HOME} not found."
+  exit 1
+fi
+if [ ! -d ${TOMCAT_HOME} ]; then
+  echo "[`date`] [ERROR] TOMCAT_HOME=${TOMCAT_HOME} not exists."
+  exit 1
+fi
+
+if [ -z "${JAVA_VM_MEM_MIN}" ]; then
+  JVM_MIN_MEM=512
+fi
+if [ -z "${JAVA_VM_MEM_MAX}" ]; then
+  JVM_MAX_MEM=1024
+fi
+if [ ${JAVA_VM_MEM_MIN} -gt ${JAVA_VM_MEM_MAX} ]; then
+  echo "[`date`] [WARN ] JAVA_VM_MEM_MIN is bigger than JAVA_VM_MEM_MAX"
+  JAVA_VM_MEM_MAX=${JAVA_VM_MEM_MIN}
+fi
+
+JAVA_OPTS="${JAVA_OPTS} -Xms${JAVA_VM_MEM_MIN}m -Xmx${JAVA_VM_MEM_MAX}m"
+JAVA_OPTS="${JAVA_OPTS} -Dfile.encoding=utf-8 -Duser.timezone=Asia/Shanghai"
+
+# Java Remote Debug Enabled
+if [ "YESX" == "${JAVA_DEBUG}" ] || [ "yesX" == "${JAVA_DEBUG}" ]; then
+    JAVA_OPTS="${JAVA_OPTS} -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8888"
+fi
+
+export JAVA_OPTS
+
+${TOMCAT_HOME}/bin/catalina.sh run "$@"
