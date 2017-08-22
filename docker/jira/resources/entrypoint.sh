b88fd4ad47d1fbefeff6779a39100737fa1c80aa
ZhongWen Li <lao-tse@qq.com>
2017-08-22 16:34:42 +0800

    Update: jira dockerfile

--- docker/jira/resources/entrypoint.sh
+++ docker/jira/resources/entrypoint.sh
@@ -17,6 +17,22 @@ if [ ! -d ${JIRA_APP} ]; then
   exit 1
 fi
 
+
+if [ -z "${JAVA_VM_MEM_MIN}" ]; then
+  JVM_MIN_MEM=512
+fi
+if [ -z "${JAVA_VM_MEM_MAX}" ]; then
+  JVM_MAX_MEM=1024
+fi
+if [ ${JAVA_VM_MEM_MIN} -gt ${JAVA_VM_MEM_MAX} ]; then
+  echo "[`date`] [WARN ] JAVA_VM_MEM_MIN is bigger than JAVA_VM_MEM_MAX"
+  JAVA_VM_MEM_MAX=${JAVA_VM_MEM_MIN}
+fi
+
+JAVA_OPTS="${JAVA_OPTS} -Xms${JAVA_VM_MEM_MIN}m -Xmx${JAVA_VM_MEM_MAX}m"
+JAVA_OPTS="${JAVA_OPTS} -Dfile.encoding=utf-8 -Duser.timezone=Asia/Shanghai"
+export JAVA_OPTS
+
 if [ -x "${JIRA_APP}/bin/catalina.sh" ]; then
   ${JIRA_APP}/bin/catalina.sh run
 else
