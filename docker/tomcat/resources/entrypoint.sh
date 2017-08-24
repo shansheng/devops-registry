4afe308eefb2528535df3de476b2de922d4d7762
ZhongWen Li <lao-tse@qq.com>
2017-08-24 09:43:02 +0800

    Update: Fix tomcat dockerfile and entrypoint

--- docker/tomcat/resources/entrypoint.sh
+++ docker/tomcat/resources/entrypoint.sh
@@ -31,8 +31,24 @@ JAVA_OPTS="${JAVA_OPTS} -Xms${JAVA_VM_MEM_MIN}m -Xmx${JAVA_VM_MEM_MAX}m"
 JAVA_OPTS="${JAVA_OPTS} -Dfile.encoding=utf-8 -Duser.timezone=Asia/Shanghai"
 
 # Java Remote Debug Enabled
-if [ "YESX" == "${JAVA_DEBUG}" ] || [ "yesX" == "${JAVA_DEBUG}" ]; then
+if [ "YESX" == "${JAVA_DEBUG}X" ] || [ "yesX" == "${JAVA_DEBUG}X" ]; then
     JAVA_OPTS="${JAVA_OPTS} -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8888"
+fi
+
+# Tomcat ThreadPool
+if [ -z "${TOMCAT_MAX_THREADS}" ]; then
+  TOMCAT_MAX_THREADS=10000
+fi
+if [ -z "${TOMCAT_MIN_SEPARE_THREADS}" ]; then
+  TOMCAT_MIN_SEPARE_THREADS=10
+fi
+# Fix server.xml
+if [ -f ${TOMCAT_HOME}/conf/server-template.xml ]; then
+  \cp -f ${TOMCAT_HOME}/conf/server-template.xml ${TOMCAT_HOME}/conf/server.xml
+  sed -i -e "s/TOMCAT_MAX_THREADS/${TOMCAT_MAX_THREADS}/g" ${TOMCAT_HOME}/conf/server.xml
+  sed -i -e "s/TOMCAT_MIN_SEPARE_THREADS/${TOMCAT_MIN_SEPARE_THREADS}/g" ${TOMCAT_HOME}/conf/server.xml
+else
+  echo "[`date`] [WARN ] Template file ${TOMCAT_HOME}/conf/server-template.xml not found."
 fi
 
 export JAVA_OPTS
